efa998d213a34e41c9c02f7d2016b72aa07ccb3c
Mehtab khan <142143274+mehtab11011@users.noreply.github.com>
2025-09-07 11:55:31 +0500

    Update models.py

--- addtask/models.py
+++ addtask/models.py
@@ -1,15 +1,7 @@
-# from django.db import models
-# class taskaddmodel(models.Model):
-#     name=models.CharField(max_length=20)
-#     Description=models.CharField(max_length=500)
-#     date=models.CharField(max_length=30)
-#     Priority=models.CharField(max_length=30)
-
-# # Create your models here.
 from django.db import models
 
 class TaskAddModel(models.Model):
-    name = models.CharField(max_length=20, blank=False, null=False)  # Required field
-    description = models.CharField(max_length=500, blank=True, null=True)  # Optional
-    date = models.CharField(max_length=30, blank=True, null=True)  # Optional
-    priority = models.CharField(max_length=30, blank=True, null=True)  # Optional
+    name = models.CharField(max_length=20, blank=False, null=False)  
+    description = models.CharField(max_length=500, blank=True, null=True) 
+    date = models.DateField(blank=True, null=True)  
+    priority = models.CharField(max_length=30, blank=True, null=True) 
